1ae43b065ac8259cda03dc3a21d7bfb18a7be37b
TheShaqtus <adam.mraz@colorado.edu>
2020-08-31 10:43:09 -0500

    Creating integration test project and removing connection string

---
+++ Volleyball.Integration.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Volleyball.Integration.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
